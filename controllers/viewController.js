eb42166472e8c57d19a1857d1951e22fa84e54b7
Nghiatruonggg <nghiatruong110700@gmail.com>
2025-01-23 17:07:38 +0700

    Added overview + tour detail page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1, Take data from collections
   const tours = await Tour.find();
-  console.log(tours)
+  // console.log(tours)
   // 2, Build the template
 
   // 3, Export page
@@ -14,8 +14,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res, next) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+  res.status(200).render('tour', {
+    title: `${tour.name}`,
+    tour,
+    reviews: tour.reviews,
+    guides: tour.guides
+  });
+});
